09831a2dea831554b1fcd0447f9179dd89757712
George Sakellaropoulos <96526516+GSake98@users.noreply.github.com>
2025-07-16 15:51:29 +0000

    shallow copy of variables

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const INITIAL_GAME_BOARD = [
   ["", "", ""],
   ["", "", ""],
@@ -5,14 +7,26 @@ const INITIAL_GAME_BOARD = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(INITIAL_GAME_BOARD);
+
+  function handleSelection(rowIndex, colIndex, playerSymbol) {
+    setGameBoard((prevGameBoard) => {
+      var updatedBoard = prevGameBoard.map((innerArray) => [...innerArray]);
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {INITIAL_GAME_BOARD.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelection(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
